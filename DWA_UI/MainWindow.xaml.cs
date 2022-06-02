c066788701189cab4d5e4d2603f2761bb35d3968
stkkkrau <katharina.kraus@student.hs-anhalt.de>
2022-06-02 13:24:06 +0200

    Dateien und Ordner in list angezeigt, pfad in textbox sichtbar

--- DWA_UI/MainWindow.xaml.cs
+++ DWA_UI/MainWindow.xaml.cs
@@ -11,7 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+using ListView = System.Windows.Controls.ListView;
+using Path = System.IO.Path;
 
 namespace DWA_UI
 {
@@ -27,12 +30,57 @@ namespace DWA_UI
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+           
         }
 
         private void ListView_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.ShowNewFolderButton = false;
+            folderDialog.RootFolder = Environment.SpecialFolder.Desktop;
+            DialogResult result = folderDialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                FileList.Items.Clear();
+                string sPath = folderDialog.SelectedPath;
+
+                FilePath.Text = sPath;
+
+               
+                try
+                {
+                    var files = Directory.EnumerateFileSystemEntries(sPath);
+                    foreach (var d in files)
+                    {
+                        FileList.Items.Add(Path.GetFileName(d));
+                    }
+                }
+                catch (System.Exception excpt)
+                {
+                    Console.WriteLine(excpt.Message);
+                }
+
+               // System.Diagnostics.Debug.WriteLine(sPath);
+
+                Properties.Settings.Default.Reload();
+            }
+        }
+
     }
+
+    class Files
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime ModificationTime { get; set; }
+        public string Owner { get; set; }
+        public string OwnerDescription { get; set; }
+    }
+
 }
