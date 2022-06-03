0adf8bb45d4acff79266047b89c1e9d07515129e
stkkkrau <katharina.kraus@student.hs-anhalt.de>
2022-06-03 08:12:37 +0200

    visual studio 2019 projekt

---
+++ WpfApp2019/Entity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateTestCase
+{
+    public class EntityAttribute
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public string DataType { get; set; }
+
+    }
+    public class Entity
+    {
+        public string Name { get; set; }
+
+        public List<EntityAttribute> Attributes;
+
+        public Entity(string Name)
+        {
+            this.Name = Name;
+            Attributes = new List<EntityAttribute> { new EntityAttribute(){ Name = "Attribute1", Type ="A", DataType="Int" },
+                                                     new EntityAttribute(){ Name = "Attribute2", Type ="A", DataType="String"  },
+                                                     new EntityAttribute(){ Name = "Attribute3", Type ="B", DataType="Date" },
+                                                     new EntityAttribute(){ Name = "Attribute4", Type ="A", DataType="String" },
+                                                     new EntityAttribute(){ Name = "SpecialAtttribute", Type ="C", DataType="String" }
+                                             };
+        }
+
+        public List<EntityAttribute> GetAttributes(string Type)
+        {
+            return Attributes.Where(attr => attr.Type == Type).ToList();
+        }
+        public EntityAttribute GetAttributeByName(string Name)
+        {
+            return Attributes.Where(attr => attr.Name == Name).First();
+        }
+
+
+    }
+}
---
+++ WpfApp2019/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.IO;
+using Path = System.IO.Path;
+using TemplateTestCase;
+using RazorEngine;
+using RazorEngine.Templating;
+
+
+namespace WpfApp2019
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.ShowNewFolderButton = false;
+            folderDialog.RootFolder = Environment.SpecialFolder.Desktop;
+            DialogResult result = folderDialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                FileList.Items.Clear();
+                string sPath = folderDialog.SelectedPath;
+
+                FilePath.Text = sPath;
+
+
+                try
+                {
+                    var files = Directory.EnumerateFileSystemEntries(sPath);
+                    foreach (var d in files)
+                    {
+                        FileList.Items.Add(Path.GetFileName(d));
+                    }
+                }
+                catch (System.Exception excpt)
+                {
+                    Console.WriteLine(excpt.Message);
+                }
+
+                // System.Diagnostics.Debug.WriteLine(sPath);
+
+                //Properties.Settings.Default.Reload();
+            }
+        }
+        private void Button_Test_Click(object sender, RoutedEventArgs e)
+        {
+
+            string templateText = File.ReadAllText("..\\..\\..\\TextFile1-NormalTemplate.txt");
+            var model = new Entity("Bert");
+            var resultText = Engine.Razor.RunCompile(templateText, "TextNormal", null, model);
+            File.WriteAllTextAsync("..\\..\\..\\WriteText.txt", resultText);
+            Console.Write(resultText);
+
+        }
+    }
+
+    class Files
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime ModificationTime { get; set; }
+        public string Owner { get; set; }
+        public string OwnerDescription { get; set; }
+    }
+}
