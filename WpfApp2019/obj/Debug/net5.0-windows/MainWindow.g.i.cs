706749e3205a133f798ebb76875b54c9dc0b70a3
stankinn <anne.kinne@student.hs-anhalt.de>
2022-06-10 17:28:29 +0200

    Sortieren und erste Dateiattribute richitg zuordnen

--- WpfApp2019/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ WpfApp2019/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace WpfApp2019 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp2019;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp2019;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
