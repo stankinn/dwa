31950208c51c542dd1637e89a6756d2388ee59e9
stvibuzo <vivienne.s.buzov@student.hs-anhalt.de>
2022-07-06 17:33:11 +0200

    TreeView hinzugefügt

--- WpfApp2019/MainPage.xaml.cs
+++ WpfApp2019/MainPage.xaml.cs
@@ -24,6 +24,39 @@ namespace WpfApp2019
         public MainPage()
         {
             InitializeComponent();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo driveInfo in drives)
+                TreeViewStructure.Items.Add(CreateTreeItem(driveInfo));
+        }
+
+        public void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = e.Source as TreeViewItem;
+            if ((item.Items.Count == 1) && (item.Items[0] is string))
+            {
+                item.Items.Clear();
+
+                DirectoryInfo expandedDir = null;
+                if (item.Tag is DriveInfo)
+                    expandedDir = (item.Tag as DriveInfo).RootDirectory;
+                if (item.Tag is DirectoryInfo)
+                    expandedDir = (item.Tag as DirectoryInfo);
+                try
+                {
+                    foreach (DirectoryInfo subDir in expandedDir.GetDirectories())
+                        item.Items.Add(CreateTreeItem(subDir));
+                }
+                catch { }
+            }
+        }
+
+        private TreeViewItem CreateTreeItem(object o)
+        {
+            TreeViewItem item = new TreeViewItem();
+            item.Header = o.ToString();
+            item.Tag = o;
+            item.Items.Add("Loading...");
+            return item;
         }
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
