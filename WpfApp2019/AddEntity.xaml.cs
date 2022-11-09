6ee42c899b2c50895215e644f32244700730b4fd
Ratteit, Friederike <friederike.ratteit@student.hs-anhalt.de>
2022-11-09 16:37:15 +0100

    Code für RazorEngingeCore angepasst

--- WpfApp2019/AddEntity.xaml.cs
+++ WpfApp2019/AddEntity.xaml.cs
@@ -3,17 +3,15 @@ using System.Windows.Controls;
 using System.IO;
 using TemplateTestCase;
 using System.Data.SqlClient;
-
+using RazorEngineCore;
 
 namespace WpfApp2019
 {
-    /// <summary>
-    /// Interaktionslogik für AddEntity.xaml
-    /// </summary>
+
     public partial class AddEntity : Page
     {
         //die verbindung muss individuell angepasst sein
-      /*    public static SqlConnection con = new SqlConnection("Data Source=DESKTOP-VD7D8FL;Initial Catalog=databaseConnection;Integrated Security=True");
+        //public static SqlConnection con = new SqlConnection("Data Source=DESKTOP-VD7D8FL;Initial Catalog=databaseConnection;Integrated Security=True");
 
         public AddEntity()
         {
@@ -22,28 +20,29 @@ namespace WpfApp2019
 
         private void Button_Add_Entity(object sender, RoutedEventArgs e)
         {
-            con.Open();
+            //con.Open();
 
-
+            IRazorEngine razorEngine = new RazorEngine();
             string templateText = File.ReadAllText("..\\..\\..\\TextFile1-NormalTemplate.txt");
             var model = new Entity("");
             model.Name = EntityName.Text;
             model.Attributes.Add(new EntityAttribute() { Name = AttributeNames.Text, Type = AttributeType.Text, DataType = AttributeDataType.Text });
             System.Diagnostics.Debug.WriteLine(model.Attributes);
-            var resultText = Engine.Razor.RunCompile(templateText, "TextNormal", null, model);
-            File.WriteAllTextAsync("..\\..\\..\\WriteText.txt", resultText);
+            IRazorEngineCompiledTemplate template = razorEngine.Compile(templateText);
+            string resultText = template.Run(model);
+            File.WriteAllText("..\\..\\..\\WriteText.txt", resultText);
             System.Diagnostics.Debug.WriteLine(resultText);
 
-            SqlCommand cmd = new SqlCommand("INSERT INTO USERS(username, email, phone) values ('" + EntityName.Text + "','" + AttributeType.Text + "','" + AttributeDataType.Text + "')", con);
-            cmd.ExecuteNonQuery();
-            con.Close();
+            //SqlCommand cmd = new SqlCommand("INSERT INTO USERS(username, email, phone) values ('" + EntityName.Text + "','" + AttributeType.Text + "','" + AttributeDataType.Text + "')", con);
+            //cmd.ExecuteNonQuery();
+            //con.Close();
         }
 
         private void Button_Go_Back(object sender, RoutedEventArgs e)
-          {
-              // View Expense Report
-              MainPage mainPage = new MainPage();
-              this.NavigationService.Navigate(mainPage);
-          }*/
+        {
+            // View Expense Report
+            View.FileView mainPage = new View.FileView();
+            this.NavigationService.Navigate(mainPage);
+        }
     }
 }
